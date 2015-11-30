c1083db972affb46d48aaf4cad0b3f4e05c44c0b
张小凡 <hyb0320@gmail.com>
2015-12-01 00:29:46 +0800

    Add tracking object for CamShift

--- hahacamera/GlassFilter.cpp
+++ hahacamera/GlassFilter.cpp
@@ -4,11 +4,16 @@
 
 Haha::GlassFilterImpl::GlassFilterImpl()
 	: Filter(FilterType::GlassFilter)
-	,_faceCascadeFileName("resources/haarcascade_frontalface_alt2.xml")
+	, _faceCascadeFileName("resources/haarcascade_frontalface_alt2.xml")
 	, _face_cascade(nullptr)
 	, _eyeCascadeFileName("resources/haarcascade_eye_tree_eyeglasses.xml")
 	, _eye_cascade(nullptr)
 	, _glass_res(cv::imread("resources/glass.png"))
+	, _missingObject(true)
+	, _trackObject(-1)
+	, _vmin(10)
+	, _vmax(256)
+	, _smin(30)
 {
 	_face_cascade = std::make_shared<cv::CascadeClassifier>();
 	auto ret = _face_cascade->load(_faceCascadeFileName);
@@ -35,7 +40,14 @@ void Haha::GlassFilterImpl::Affect(cv::Mat& img)
 
 	if (faces.size() >= 1) {
 		std::vector<cv::Rect> eyes;
-		DetectionEye(img(faces[0]), eyes);
+		auto faceX = faces[0].x <= 0 ? 0 : faces[0].x;
+		faceX = faceX > img.cols ? img.cols : faceX;
+		auto faceY = faces[0].y <= 0 ? 0 : faces[0].y;
+		faceY = faceY > img.rows ? img.rows : faceY;
+		auto faceWidth = faceX + faces[0].width >= img.cols ? img.cols - faceX : faces[0].width;
+		auto faceHeight = faceY + faces[0].height >= img.rows ? img.rows - faceY : faces[0].height;
+		auto faceRect = cv::Rect(faceX, faceY, faceWidth, faceHeight);
+		DetectionEye(img(faceRect), eyes);
 
 		if (eyes.size() >= 2) {
 			auto eye_center1 = cv::Point2f(faces[0].x + eyes[0].x + static_cast<float>( eyes[0].width ) / 2, faces[0].y + eyes[0].y + static_cast<float>( eyes[0].height ) / 2);
@@ -67,11 +79,71 @@ void Haha::GlassFilterImpl::Affect(cv::Mat& img)
 bool Haha::GlassFilterImpl::DetectionFace(cv::Mat& img, std::vector<cv::Rect>& faces)
 {
 	if (nullptr == _face_cascade || _face_cascade->empty()) { return false; }
-	cv::Mat gray_img;
-	cvtColor(img, gray_img, CV_BGR2GRAY);
-	equalizeHist(gray_img, gray_img);
-	_face_cascade->detectMultiScale(img, faces, 1.1, 3, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30), cv::Size(0, 0));
-	return true;
+	if (_missingObject) //use CascadeClassifier
+	{
+		cv::Mat gray_img;
+		cvtColor(img, gray_img, CV_BGR2GRAY);
+		equalizeHist(gray_img, gray_img);
+		_face_cascade->detectMultiScale(img, faces, 1.1, 3, 0 | CV_HAAR_SCALE_IMAGE, cv::Size(30, 30), cv::Size(0, 0));
+		if (faces.size() >= 1)
+		{
+			_missingObject = false;
+			_trackObject = -1;
+			_lastRect = cv::Rect(faces[0]);
+		}
+		return true;
+	}
+	else //use CamShift
+	{
+		cv::Mat hsv_img, mask_img, histimg_img = cv::Mat::zeros(200, 320, CV_8UC3);
+		const int hsize = 16;
+		float hranges[] = { 0,180 };
+		const float* phranges = hranges;
+		cv::cvtColor(img, hsv_img, CV_BGR2HSV); //convert image color to HSV from BGR
+		cv::inRange(hsv_img, cv::Scalar(0, _smin, std::min(_vmin, _vmax)),
+			cv::Scalar(180, 256, std::max(_vmin, _vmax)), mask_img);
+		int ch[] = { 0, 0 };
+		_hue_img.create(hsv_img.size(), hsv_img.depth());
+		cv::mixChannels(&hsv_img, 1, &_hue_img, 1, ch, 1);
+
+		if (_trackObject < 0)
+		{
+			cv::Mat roi(_hue_img, _lastRect), maskroi(mask_img, _lastRect);
+			calcHist(&roi, 1, 0, maskroi, _hist_img, 1, &hsize, &phranges);
+			cv::normalize(_hist_img, _hist_img, 0, 255, CV_MINMAX);
+			_trackObject = 1;
+			histimg_img = cv::Scalar::all(0);
+			int binW = histimg_img.cols / hsize;
+			cv::Mat buf(1, hsize, CV_8UC3);
+			for (int i = 0; i < hsize; i++)
+			{
+				buf.at<cv::Vec3b>(i) = cv::Vec3b(cv::saturate_cast<uchar>(i*180. / hsize), 255, 255);
+			}
+			cv::cvtColor(buf, buf, CV_HSV2BGR);
+		}
+
+		cv::calcBackProject(&_hue_img, 1, 0, _hist_img, _backproj_img, &phranges);
+		_backproj_img &= mask_img;
+
+		cv::RotatedRect trackBox = cv::CamShift(_backproj_img, _lastRect,
+			cv::TermCriteria(CV_TERMCRIT_EPS | CV_TERMCRIT_ITER, 10, 1));
+		if (trackBox.size.width < 60 || trackBox.size.height < 60)
+		{
+			_trackObject = -1;
+			_missingObject = true;
+		}
+		if (_lastRect.area() <= 1)
+		{
+			int cols = _backproj_img.cols, rows = _backproj_img.rows, r = (std::min(cols, rows) + 5) / 6;
+			_lastRect = cv::Rect(_lastRect.x - r, _lastRect.y - r,
+				_lastRect.x + r, _lastRect.y + r) &
+				cv::Rect(0, 0, cols, rows);
+		}
+
+		cv::ellipse(img, trackBox, cv::Scalar(0, 200, 255), 3, CV_AA);	//draw face for track object
+		faces.push_back(cv::Rect(trackBox.boundingRect()));
+		return true;
+	}
 }
 
 bool Haha::GlassFilterImpl::DetectionEye(cv::Mat& img, std::vector<cv::Rect>& eyes)
--- hahacamera/GlassFilter.hpp
+++ hahacamera/GlassFilter.hpp
@@ -19,5 +19,14 @@ namespace Haha
 		std::shared_ptr<cv::CascadeClassifier> _eye_cascade;
 		const std::string _eyeCascadeFileName;
 		cv::Mat _glass_res;
+		cv::Rect _lastRect;
+		int _vmin;
+		int _vmax;
+		int _smin;
+		bool _missingObject;
+		int _trackObject;
+		cv::Mat _hue_img;
+		cv::Mat _hist_img;
+		cv::Mat _backproj_img;
 	};
 }
